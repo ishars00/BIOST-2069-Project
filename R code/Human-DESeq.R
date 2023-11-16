44215cf9d69f3841204f3f8b40ec5e29c5fe5679
mwc <43445389+mwc10@users.noreply.github.com>
2023-11-16 11:50:42 -0500

    update human DESeq2 script to match number that paper calculated

--- R code/Human-DESeq.R
+++ R code/Human-DESeq.R
@@ -2,59 +2,67 @@
 ######## Libraries ########
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-# BiocManager::install(version = "3.18")
 
-## For fetching data from GEO
-BiocManager::install("GEOquery")
-
-
-library(GEOquery)
 library(tidyverse)
 library(here)
 library(DESeq2)
 library(EnhancedVolcano)
 
 # helpers
-
 to_named <- function(df, converter, rowcol) {
   res = converter(df |> select(!{{rowcol}}))
   rownames(res) = df |> pull({{rowcol}})
   return (res)
 }
 
-# First, get the data from GEO using BioConductor
-# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE167536
-# geo_accension = 'GSE167536'
-# gds = getGEO(geo_accension, GSEMatrix=FALSE)
-
 
 hmn_expr = read_csv(here('GSE167536', 'GSE167536_humanstress_rawcounts.txt')) |>
   to_named(as.matrix, 'gene')
 
+hmn_mi = read_csv(here('GSE167536', 'BioSample', 'human_mi_status.csv')) |>
+  dplyr::rename(SampleName = sample_name) |>
+  select(SampleName, MI_status) |>
+  mutate(MI_status = relevel(as.factor(MI_status), 'control'))
+
 hmn_samples = read_csv(here('GSE167536', 'BioSample', 'human_sample_info.csv')) |>
   mutate(group = relevel(as.factor(str_replace(group, ' ', '_')), 'low_stress')) |>
   dplyr::rename(provider = `biomaterial provider`) |>
+  left_join(hmn_mi, by = join_by(SampleName)) |>
   to_named(as.data.frame, 'SampleName') |>
   (function(df, o) df[o, ])(colnames(hmn_expr)) |>
   AnnotatedDataFrame()
 
 hmn_eset = ExpressionSet(hmn_expr, hmn_samples)
 hmn_se = makeSummarizedExperimentFromExpressionSet(hmn_eset)
-hmn_dss = DESeqDataSet(hmn_se, ~ group)
+# from https://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html:
+#   In order to benefit from the default settings of the package, 
+#   you should put the variable of interest at the end of the formula 
+#   and make sure the control level is the first level.
+# this is different than what they had in the paper
+# hmn_dss = DESeqDataSet(hmn_se, ~ group + MI_status)
+hmn_dss = DESeqDataSet(hmn_se, ~ MI_status + group)
 
 # do not filter the genes?
 
-# levels set when reading data
+# proper reference levels were set when reading in the data
+# and the design is set up to compare for the stress group
+# But, maybe better if this was explicit in the call?
 hmn_dss = DESeq(hmn_dss)
 
 # they used pvalue < 0.05 for human DE genes
-hmn_res = results(hmn_dss, contrast = c('group', 'low_stress', 'high_stress'), alpha=0.05)
+hmn_contrast = c('group', 'low_stress', 'high_stress')
+hmn_res = results(hmn_dss, contrast = hmn_contrast)
 hmn_res = hmn_res[complete.cases(hmn_res),]
-EnhancedVolcano(hmn_res, lab=rownames(hmn_res), x='log2FoldChange', y='pvalue',
+EnhancedVolcano(hmn_res, 
+                lab=rownames(hmn_res), 
+                x='log2FoldChange', 
+                y='pvalue',
                 pCutoff = 0.05)
+
 
 # results to match with the paper's Sup Fig 7
 paper_res = hmn_res[hmn_res$pvalue < 0.05,]
+nrow(paper_res) # should be 1959
 
 
 
